0a2b670524dd6ce0ba39f1e03c21a2222d4d873a
zhiqianZ <63725891+zhiqianZ@users.noreply.github.com>
2025-11-03 12:19:26 -0800

    Update visualize_utils.R

--- utils/visualize_utils.R
+++ utils/visualize_utils.R
@@ -12,7 +12,7 @@ SeuratUMAP = function(args, n.pcs=20, n.cores = 10){
   seurat.obj <- read_seurat(args$integrate.ad)
   seurat.obj = RunUMAP(seurat.obj, dims = 1:n.pcs, 
                        reduction="integrated",
-                       umap.method = "umap-learn",
+                       #umap.method = "umap-learn",
                        n_threads = n.cores)
   Embeddings(seurat.obj,"umap")
 }
