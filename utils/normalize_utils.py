696836d434606110f400e41ecb3d4d86b16096ec
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-11-04 12:06:52 -0800

    Update and rename normalization_utils.py to normalize_utils.py

--- utils/normalize_utils.py
+++ utils/normalize_utils.py
@@ -1,6 +1,7 @@
 import scanpy as sc
 import anndata as ad
 import numpy as np
+import scipy as sp
 
 def log1pCPMedian(args):
     print("Running log1pCPMedian")
@@ -46,3 +47,30 @@ def PFlog1pPF(adata):
         adata.layers[new_layer] = norm
       
     return adata
+
+#### Contribute to xxx
+def do_pf(mtx, sf = None):
+    pf = mtx.sum(axis=1).A.ravel()
+    if not sf:
+        sf = pf.mean()
+    pf = sp.sparse.diags(sf/pf) @ mtx
+    return pf
+
+def norm_pf(mtx):
+    return do_pf(mtx)
+
+def norm_log_pf(mtx):
+    pf_log = np.log1p(do_pf(mtx))
+    return pf_log
+
+def norm_pf_log_pf(mtx):
+    pf_log_pf = do_pf(np.log1p(do_pf(mtx)))
+    return pf_log_pf
+   
+def logPF(counts):
+  norm = norm_log_pf(sp.sparse.csr_matrix(counts))
+  return norm.T
+ 
+def PFlogPF(counts):
+  norm = norm_pf_log_pf(sp.sparse.csr_matrix(counts))
+  return norm.T
