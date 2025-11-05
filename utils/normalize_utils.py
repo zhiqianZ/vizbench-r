8ce9a1cde230bc1742ff9bc24d871b5b4599bbb4
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-11-05 15:19:16 -0800

    Update normalize_utils.py

--- utils/normalize_utils.py
+++ utils/normalize_utils.py
@@ -3,6 +3,64 @@ import anndata as ad
 import numpy as np
 import scipy as sp
 
+def log1pCPMedian(args):
+    print("Running log1pCPMedian")
+
+    adata_path = args['simulate_ad'] if isinstance(args, dict) else args.simulate_ad
+    adata = sc.read_h5ad(adata_path)
+
+    X_norm = np.zeros_like(adata.X, dtype=np.float32)
+    for batch in adata.obs['batch'].unique():
+        batch_idx = adata.obs['batch'] == batch
+        sub = adata[batch_idx].copy()
+        sc.pp.normalize_total(sub, target_sum=None, inplace=True)
+        sc.pp.log1p(sub)
+        X_norm[batch_idx, :] = sub.X
+    adata.layers["data"] = X_norm
+  
+    return adata
+
+
+def log1pPF(args):
+    print("Running log1pPF")
+
+    adata_path = args['simulate_ad'] if isinstance(args, dict) else args.simulate_ad
+    adata = sc.read_h5ad(adata_path)
+
+    X_norm = np.zeros_like(adata.X, dtype=np.float32)
+
+    for batch in adata.obs['batch'].unique():
+        batch_idx = adata.obs['batch'] == batch
+        sub = adata[batch_idx].copy()
+
+        X_batch = sub.X.toarray() if sp.issparse(sub.X) else sub.X
+        X_norm_batch = logPF(X_batch)
+        X_norm[batch_idx, :] = X_norm_batch
+
+    adata.layers["data"] = X_norm
+
+    return adata
+
+def log1pPF(args):
+    print("Running log1pPF")
+
+    adata_path = args['simulate_ad'] if isinstance(args, dict) else args.simulate_ad
+    adata = sc.read_h5ad(adata_path)
+
+    X_norm = np.zeros_like(adata.X, dtype=np.float32)
+
+    for batch in adata.obs['batch'].unique():
+        batch_idx = adata.obs['batch'] == batch
+        sub = adata[batch_idx].copy()
+
+        X_batch = sub.X.toarray() if sp.issparse(sub.X) else sub.X
+        X_norm_batch = PFlogPF(X_batch)
+        X_norm[batch_idx, :] = X_norm_batch
+
+    adata.layers["data"] = X_norm
+
+    return adata
+    
 #### Contribute to xxx
 def do_pf(mtx, sf = None):
     pf = mtx.sum(axis=1).A.ravel()
