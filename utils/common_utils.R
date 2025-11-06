aa1919cae178cbf3642341ba9f4d81324f7e7c0d
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-11-05 18:57:25 -0800

    Update common_utils.R

--- utils/common_utils.R
+++ utils/common_utils.R
@@ -12,6 +12,8 @@ write_ad <- function(x, file, verbose = TRUE) {
   if(verbose) message(paste("Converting", class(x), "-> AnnData."))
   if(typeof(x)!="environment"){
     x.ad <- as_AnnData(x)
+  }else{
+    x.ad <- x
   }
   if(verbose) message(paste0("Writing: ", file, "."))
   write_h5ad(x.ad, fn, mode = "w", compression = "gzip")
