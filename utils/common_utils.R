23afb18e7500acdf27aea6f1a83770eeae90ad78
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-11-05 19:31:49 -0800

    Update common_utils.R

--- utils/common_utils.R
+++ utils/common_utils.R
@@ -8,21 +8,11 @@ suppressPackageStartupMessages({
 read_sce <- function(f) read_h5ad(f, as = "SingleCellExperiment")
 read_seurat <- function(f) read_h5ad(f, as = "Seurat")
 
-write_ad <- function(x, file, verbose = TRUE) {
+write_seurat_ad <- function(x, file, verbose = TRUE) {
   if(verbose) message(paste("Converting", class(x), "-> AnnData."))
-  if(typeof(x)!="environment"){
     x.ad <- as_AnnData(x)
     if(verbose) message(paste0("Writing: ", file, "."))
     write_h5ad(x.ad, fn, mode = "w", compression = "gzip")
-  }else{
-    if(verbose) message(paste0("Writing: ", file, "."))
-    x$write(
-    fn,
-    compression='gzip'
-    )
-  }
-  if(verbose) message("Done.")
-}
 
 read_normmethod <- function(f) fromJSON(paste(readLines(f), 
                                               collapse=""))$normalize
