e9be2c4eef143191fb537c9915e14f8d56bf627b
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-11-05 16:20:40 -0800

    Update normalize_utils.R

--- utils/normalize_utils.R
+++ utils/normalize_utils.R
@@ -1,28 +1,8 @@
 
 load_pkgs <- function() {
   library(Seurat)
-  library(distances)
-  library(dplyr)
 }
 
-#use_condaenv("Benchmark")
-# MR: comment out python for now; TODO: build/integrate reticulate env
-# sc = import("scanpy", convert=F)
-# source_python("utils/normalization.py")
-
-# Normalization = function(seurat.obj, NormMethod){
-#   seurat.obj = NormMethod(seurat.obj)
-#   return(seurat.obj)
-# }
-
-# old version
-# log1pCP10k = function(seurat.obj){
-#   print("Running log1pCP10k")
-#   seurat.obj = NormalizeData(seurat.obj,scale.factor = 10^4)
-#   return(seurat.obj)
-# }
-
-# new version
 log1pCP10k = function(args){
   message("Running log1pCP10k")
   seurat.obj <- read_seurat(args$simulate.ad)
@@ -40,75 +20,10 @@ sctransform = function(args){
   seurat.obj <- read_seurat(args$simulate.ad)
   nhvgs = args$nhvgs
   seurat.obj[["RNA"]] <- split(seurat.obj[["RNA"]], f = seurat.obj$batch)
-  SCTransform(seurat.obj, vst.flavor = "v2", verbose = FALSE, 
-              variable.features.n = nhvgs, method = "glmGamPoi")
+  seurat.obj = SCTransform(seurat.obj, vst.flavor = "v2", verbose = FALSE, 
+              variable.features.n = nhvgs, method = "glmGamPoi",min_cells=0)
+  return(seurat.obj)
 }
-
-# log1pPF = function(args){
-#   message("Running log1pPF")
-#   seurat.obj <- read_seurat(args$simulate.ad)
-#   counts.layer = Layers(object = seurat.obj, search = "counts")
-#   save <- make.unique(names = gsub(
-#     pattern = "counts",
-#     replacement = "data",
-#     x = counts.layer
-#   ))
-#   for (i in seq_along(along.with = counts.layer)) {
-#     l <- counts.layer[i]
-#     LayerData(
-#       object = seurat.obj,
-#       layer = save[i],
-#       features = Features(x = seurat.obj, layer = l),
-#       cells = Cells(x = seurat.obj, layer = l)
-#     ) <- logPF(t(LayerData(object = seurat.obj, layer = l, fast = NA)))
-#   }
-#   return(seurat.obj)
-# }
-# 
-# PFlog1pPF = function(args){
-#   message("Running PFlog1pPF")
-#   seurat.obj <- read_seurat(args$simulate.ad)
-#   counts.layer = Layers(object = seurat.obj, search = "counts")
-#   save <- make.unique(names = gsub(
-#     pattern = "counts",
-#     replacement = "data",
-#     x = counts.layer
-#   ))
-#   for (i in seq_along(along.with = counts.layer)) {
-#     l <- counts.layer[i]
-#     LayerData(
-#       object = seurat.obj,
-#       layer = save[i],
-#       features = Features(x = seurat.obj, layer = l),
-#       cells = Cells(x = seurat.obj, layer = l)
-#     ) <- PFlogPF(t(LayerData(object = seurat.obj, layer = l, fast = NA)))
-#   }
-#   return(seurat.obj)
-# }
-
-# TODO: rewrite this in native Python
-# `log1pCPMedian` = function(seurat.obj){
-#   print("Running Scanpy default normalization")
-#   counts.layer = Layers(object = seurat.obj, search = "counts")
-#   save <- make.unique(names = gsub(
-#     pattern = "counts",
-#     replacement = "data",
-#     x = counts.layer
-#   ))
-#   for (i in seq_along(along.with = counts.layer)) {
-#     l <- counts.layer[i]
-#     count = t(LayerData(object = seurat.obj, layer = l, fast = NA))
-#     adata = sc$AnnData(count)
-#     norm = log1p(py_to_r(sc$pp$normalize_total(adata,inplace=F)$X))
-#     LayerData(
-#       object = seurat.obj,
-#       layer = save[i],
-#       features = Features(x = seurat.obj, layer = l),
-#       cells = Cells(x = seurat.obj, layer = l)
-#     ) <- t(norm)
-#   }
-#   return(seurat.obj)
-# }
 
 
 
