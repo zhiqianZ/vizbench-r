aa3bc4f5bf81d77df106ab26cc8578f1aaaf4b89
Zhiqian Zhai <63725891+zhiqianZ@users.noreply.github.com>
2025-12-10 16:32:34 -0800

    Update integrate_utils.R

--- utils/integrate_utils.R
+++ utils/integrate_utils.R
@@ -83,64 +83,62 @@ FastMNNIntegration <- function(
 }
 
 
-labels_new <- function(object, values, select = c("first", "last", "common", "all"), simplify = TRUE, ...) {
-  message("Using new labels")
-  select <- select[1L]
-  select <- match.arg(arg = select)
-  values <- intersect(values, rownames(object))
-  
-  if (length(values) == 0) {
-    return(character(0))
+RunCCA.default <- function(
+  object1,
+  object2,
+  standardize = TRUE,
+  num.cc = 20,
+  seed.use = 42,
+  verbose = FALSE,
+  ...
+) {
+  print("new")
+  if (!is.null(x = seed.use)) {
+    set.seed(seed = seed.use)
   }
-  
-  # Precompute logical matrix and column names
-  cmap_data <- as.matrix(object)
-  colnames_object <- colnames(object)
-  rownames_object <- rownames(object)
-  
-  # Get row indices for values
-  row_indices <- match(values, rownames_object)
-  
-  # Initialize the list to store results
-  obs <- vector("list", length(values))
-  names(obs) <- values
-  
-  # Direct indexing to replace sapply
-  for (i in seq_along(row_indices)) {
-    row_idx <- row_indices[i]
-    vals <- colnames_object[cmap_data[row_idx, , drop = FALSE]]
-    if (length(vals) > 0) {
-      obs[[i]] <- vals
+  cells1 <- colnames(x = object1)
+  cells2 <- colnames(x = object2)
+  print("here1")
+  message("here1")
+  if (standardize) {
+    print("here2")
+    message("here2")
+    object1 <- Standardize(mat = object1, display_progress = FALSE)
+    object2 <- Standardize(mat = object2, display_progress = FALSE)
+  }
+  print("here3")
+  message("here3")
+  mat3 <- crossprod(x = object1, y = object2)
+  print("here4")
+  message("here4")
+  print(dim(mat3))
+  message(dim(mat3))
+  saveRDS(mat3, "mat3.rds")
+  message("done")
+  cca.svd <- irlba(A = mat3, nv = num.cc)
+  print("here5")
+  message("here5")
+  cca.data <- rbind(cca.svd$u, cca.svd$v)
+  colnames(x = cca.data) <- paste0("CC", 1:num.cc)
+  rownames(cca.data) <- c(cells1, cells2)
+  print("here6")
+  message("here6")
+  cca.data <- apply(
+    X = cca.data,
+    MARGIN = 2,
+    FUN = function(x) {
+      if (sign(x[1]) == -1) {
+        x <- x * -1
+      }
+      return(x)
     }
-  }
-  
-  obs <- Filter(length, obs)
-  
-  obs <- switch(select, 
-                first = lapply(obs, `[[`, 1L), 
-                last = lapply(obs, function(x) x[[length(x)]]), 
-                common = {
-                  counts <- table(unlist(obs))
-                  tmp <- obs
-                  obs <- vector("character", length(tmp))
-                  names(obs) <- names(tmp)
-                  for (i in seq_along(obs)) {
-                    obs[i] <- names(which.max(counts[names(counts) %in% tmp[[i]]]))
-                  }
-                  obs
-                }, 
-                obs)
-  
-  if (isTRUE(simplify)) {
-    tmp <- obs
-    obs <- unlist(tmp)
-    names(obs) <- make.unique(rep(names(tmp), times = lengths(tmp)))
-  }
-  
-  return(obs)
+  )
+  print("here7")
+  message("here7")
+  return(list(ccv = cca.data, d = cca.svd$d))
 }
-orig_labels = labels
-godmode:::assignAnywhere("labels", labels_new)
+
+godmode:::assignAnywhere("RunCCA.default", RunCCA.default)
 godmode:::assignAnywhere("FastMNNIntegration", FastMNNIntegration)
 
                 
